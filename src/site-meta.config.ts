df7b8584eb000108efdf08338a1303fdb04e13fd
Danijel Tomić <danijeltomic2004@gmail.com>
2022-09-10 10:04:44 +0200

    index page

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,11 +1,12 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
-  themeColorLight: "#fafafa",
-  themeColorDark: "#1d1f21",
-}
+	title: "Developer & Entropenuer",
+	description:
+		"Hi my name is Danijel. I am a young Developer/Entropenuer from Slovenia. I am currently working at my startup Škrinca.",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	githubUrl: "https://github.com/danimc55",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
 
 export default siteMeta;
